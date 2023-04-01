82c127672e58363ef5dfaa85c1b79fe23fcf148e
AP <a.parvez85@gmail.com>
2023-04-01 08:17:46 +0100

    adding assets folder

---
+++ assets/js/script.js
@@ -0,0 +1,52 @@
+//  global variables
+
+var timeBlock = [
+ {  
+    label: "09:00 AM",
+    tValue: "9",
+    userinput: ""
+  },
+  {
+    label: "10:00 AM",
+    tValue: "10",
+    userInput: "",
+  },
+
+  {  
+    label: "11:00 AM",
+    tValue: "11",
+    userinput: ""
+  },
+
+  {  
+    label: "12:00 PM",
+    tValue: "12",
+    userinput: ""
+  },
+  {  
+    label: "01:00 PM",
+    tValue: "13",
+    userinput: ""
+  },
+  {  
+    label: "14:00 PM",
+    tValue: "14",
+    userinput: ""
+  },
+  {  
+    label: "15:00 PM",
+    tValue: "15",
+    userinput: ""
+  },
+  {  
+    label: "16:00 PM",
+    tValue: "16",
+    userinput: ""
+  }, {  
+    label: "17:00 PM",
+    tValue: "17",
+    userinput: ""
+  }, 
+
+]
+console.log(timeBlock)
